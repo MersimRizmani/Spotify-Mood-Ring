93b52c62df43244b822d51bf7ddaa386a10d5ddc
araghu9 <54913635+araghu9@users.noreply.github.com>
2023-11-20 19:37:00 -0600

    basic code python

--- compute_mood.py
+++ compute_mood.py
@@ -1,1 +1,20 @@
 from pyodide import nltk 
+from pyodide import bs4
+from pyodide.http import pyfetch
+from spotify_auth import auth
+
+async def spotify_creds():
+    bearer_token = auth()
+    response = await pyfetch(url="https://api.spotify.com/v1/me/player/currently-playing", headers={"Authorization": f"Bearer {bearer_token}"}).json()
+    return (response["item"]["name"], response["item"]["artists"][0]["name"])
+
+async def genius_link(spotify_out):
+    bearer_token = auth()
+    response = await pyfetch(url=f"https://api.genius.com/search?q={spotify_out[0]}", headers={"Authorization": f"Bearer {bearer_token}"}).json()
+     
+    return response["item"]["name"]
+
+def lyrics_scrape():
+
+
+
---
+++ spotify_auth.py
@@ -0,0 +1,3 @@
+NEEDS TO BE IMPLEMENTED VIA https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow
+
+def auth()
