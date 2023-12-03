f555a00cf4923b7432e77f90b03f66068f642b48
araghu9 <54913635+araghu9@users.noreply.github.com>
2023-12-02 19:51:06 -0600

    tested genius API process, works. Removed extra log statemenets and implemented api error handling

--- backend.js
+++ backend.js
@@ -1,19 +1,36 @@
-const Client_id="";
-const redirect_uri = "";
-const scope = "user-read-currently-playing";
-const authUrl = new URL("https://accounts.spotify.com/authorize")
-const show_dialog = "true"
-let sp_token = ""
+// Spotify parameters needed for authorization
+const Client_id=""; //Enter App Client id for spotify here
+const redirect_uri = ""; //enter extension link, in format "https://<extension id>.chromiumapp.org/"
+const scope = "user-read-currently-playing"; //scope of api permissions, just reading current track here
+const authUrl = new URL("https://accounts.spotify.com/authorize") //spotify auth url
+const show_dialog = "true" // forces user approval everytime 
+let sp_token = ""  // auth token from implicit grant will be stored here
+const ge_token = "" // get client api token from genius app
 
+// Random state generator, adapted from spotify api documentation
 function generateRandomString(length){
     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const values = crypto.getRandomValues(new Uint8Array(length));
     return values.reduce((acc, x) => acc + possible[x % possible.length], "");
 };
 
+// Example API call for genius
+async function apiCheck(){
+    const resp = await fetch("https://api.genius.com/search?q=Imagine", {
+        method: "GET",
+        headers: {
+            "Authorization": "Bearer "+ge_token
+        }
+    });
+    return resp.json();
+}
+
+// When sign in is clicked, the component script hits the service worker, runs this to start auth work
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>{
     if (message==="signin"){
         let state = generateRandomString(16);
+
+        // Generating URL for auth with appropriate parameters for implicit grant
         var url = 'https://accounts.spotify.com/authorize';
         url += '?response_type=token';
         url += '&client_id=' + encodeURIComponent(Client_id);
@@ -21,19 +38,19 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>{
         url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
         url += '&state=' + encodeURIComponent(state);
         url += '&show_dialog=' + encodeURIComponent(show_dialog);
-        console.log(url)
 
+        // Allows pop up window to get user permission for api calls on there behalf
         chrome.identity.launchWebAuthFlow({interactive: true, url: url}, function(redirected_url){
-            console.log(redirected_url)
+            // callback from oauth2 service, saving the parameters into a dictionary style format for easy access
             let params = new URLSearchParams(redirected_url)
-            console.log(params)
-            if (redirected_url.includes("error")){
-                console.log("oops")
+            // checks for access denied or runtime errors
+            if (chrome.runtime.lastError || redirected_url.includes("error")){
                 sendResponse({message: "error"})
             }
             else{
+                // verifies state to access appropriate call
                 if (params.get("state")===state){
-                    console.log("yay")
+                    // saves the token locally, only valid as long as the extension session is open 
                     sp_token = params.get("access_token")
                     sendResponse({message: "success"})
                 }
--- popup.js
+++ popup.js
@@ -1,6 +1,11 @@
+// Button listener for log in, talks to the service worker. Console statement indicates if sign in was successful or not
 document.getElementById("signin").addEventListener('click', function(){
     chrome.runtime.sendMessage("signin", resp =>{
-        if (resp.message==="success")
-            console.log("signed in");
+        if (resp.message==="success"){
+            console.log("Signed in");
+        }
+        else{
+            console.log("Sign in unsuccessful, try again")
+        }
     })
 });
